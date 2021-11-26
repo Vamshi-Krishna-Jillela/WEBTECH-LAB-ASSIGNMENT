0062c67cdf6c7de48cc5a26589536f685954fa9d
Vamshi-Krishna-Jillela <89860169+Vamshi-Krishna-Jillela@users.noreply.github.com>
2021-11-26 18:53:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+var person1 = new Object();//creating object named person1
+var person2 = new Object();//creating object named person2
+var person3 = new Object();//creating object named person3
+
+person1.firstname = "vamshi";
+person1.lastname = "krishna";
+person1.officeAdress = "patna";
+
+person2.firstname = "kumar";
+person2.lastname = "harsh";
+person2.officeAdress = "gaya";
+
+person3.firstname = "jayanth";
+person3.lastname = "raj";
+person3.officeAdress = "Andra pradesh";
+
+console.log("firstname of person1 is "+person1.firstname);
+console.log("lastname of person1 is "+person1.lastname);
+console.log("officeAdress of person1 is "+person1.officeAdress);
+
+console.log("firstname of person2 is "+person2.firstname);
+console.log("lastname of person2 is "+person2.lastname);
+console.log("officeAdress of person2 is "+person2.officeAdress);
+
+console.log("firstname of person3 is "+person3.firstname);
+console.log("lastname of person3 is "+person3.lastname);
+console.log("officeAdress of person3 is "+person3.officeAdress);
+
